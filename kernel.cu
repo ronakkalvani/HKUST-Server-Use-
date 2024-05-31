58d83e9f0e84e515d6ac3a1e9f4326632d836b50
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-05-31 13:19:53 +0800

    Add files via upload

---
+++ kernel.cu
@@ -0,0 +1,19 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <cuda.h>
+#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("Hello World from GPU!\n");
+}
+
+int main() {
+    printf("Hello World from CPU!\n");
+    cuda_hello<<<1,1>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
+
+
