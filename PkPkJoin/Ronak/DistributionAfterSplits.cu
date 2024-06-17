dd11039d7ad3e5a9639a4398567085fc9110c710
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-06-17 19:19:01 +0800

    c

--- PkPkJoin/Ronak/DistributionAfterSplits.cu
+++ PkPkJoin/Ronak/DistributionAfterSplits.cu
@@ -68,49 +68,6 @@ __global__ void mergePartitions(
     }
 }
 
-void merge(int* h_subarrays, int* h_pivots, int n, int p) {
-    // Device pointers
-    int *d_subarrays, *d_output, *d_pivots, *d_partition_counts;
-
-    // Allocate device memory
-    CUDA_CHECK(cudaMalloc(&d_subarrays, n * sizeof(int)));
-    CUDA_CHECK(cudaMalloc(&d_output, n * sizeof(int)));
-    CUDA_CHECK(cudaMalloc(&d_pivots, (p - 1) * sizeof(int)));
-    CUDA_CHECK(cudaMalloc(&d_partition_counts, p * sizeof(int)));
-
-    // Copy data to device
-    CUDA_CHECK(cudaMemcpy(d_subarrays, h_subarrays, n * sizeof(int), cudaMemcpyHostToDevice));
-    CUDA_CHECK(cudaMemcpy(d_pivots, h_pivots, (p - 1) * sizeof(int), cudaMemcpyHostToDevice));
-    CUDA_CHECK(cudaMemset(d_partition_counts, 0, p * sizeof(int)));
-
-    // Kernel launch parameters
-    int blockSize = 256;
-    int numBlocks = (n + blockSize - 1) / blockSize;
-
-    // Launch kernel to merge partitions
-    mergePartitions<<<numBlocks, blockSize>>>(d_subarrays, d_partition_counts, d_output, d_pivots, d_partition_counts, n, p);
-    CUDA_CHECK(cudaGetLastError());
-    CUDA_CHECK(cudaDeviceSynchronize());
-
-    // Copy result back to host
-    int* h_output = new int[n];
-    CUDA_CHECK(cudaMemcpy(h_output, d_output, n * sizeof(int), cudaMemcpyDeviceToHost));
-
-    // Print result
-    for (int i = 0; i < n; ++i) {
-        std::cout << h_output[i] << " ";
-    }
-    std::cout << std::endl;
-
-    // Free device memory
-    CUDA_CHECK(cudaFree(d_subarrays));
-    CUDA_CHECK(cudaFree(d_output));
-    CUDA_CHECK(cudaFree(d_pivots));
-    CUDA_CHECK(cudaFree(d_partition_counts));
-
-    delete[] h_output;
-}
-
 int main() {
     // Example data
     // int h_subarrays[] = {1, 3, 5, 7, 2, 4, 6, 8};
