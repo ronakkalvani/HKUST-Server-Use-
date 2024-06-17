4b95c8abe91807f462b6c53dd06b321d7e445c9f
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-06-17 20:14:13 +0800

    c

--- PkPkJoin/Ronak/SortDataBlockWise.cu
+++ PkPkJoin/Ronak/SortDataBlockWise.cu
@@ -42,42 +42,42 @@ __global__ void BlockSortKernel(int *d_in, int *d_out, int num_elements)
     BlockStoreT(temp_storage.store).Store(d_out + block_offset, thread_keys, valid_items);
 }
 
-int main() {
-    // Initialize host data
-    // std::vector<int> h_data = {34, 78, 12, 56, 89, 21, 90, 34, 23, 45, 67, 11, 23, 56, 78, 99, 123, 45, 67, 89, 23, 45, 67, 34, 78};
-    std::vector<int> h_data(786);
-    for (int i=0;i<h_data.size();i++) {
-        h_data[i]=rand()%37;
-    }
-    int n = h_data.size();
+// int main() {
+//     // Initialize host data
+//     // std::vector<int> h_data = {34, 78, 12, 56, 89, 21, 90, 34, 23, 45, 67, 11, 23, 56, 78, 99, 123, 45, 67, 89, 23, 45, 67, 34, 78};
+//     std::vector<int> h_data(786);
+//     for (int i=0;i<h_data.size();i++) {
+//         h_data[i]=rand()%37;
+//     }
+//     int n = h_data.size();
 
-    // Allocate device memory
-    int* d_data;
-    cudaMalloc(&d_data, n * sizeof(int));
-    int* d_sorted_data;
-    cudaMalloc(&d_sorted_data, n * sizeof(int));
+//     // Allocate device memory
+//     int* d_data;
+//     cudaMalloc(&d_data, n * sizeof(int));
+//     int* d_sorted_data;
+//     cudaMalloc(&d_sorted_data, n * sizeof(int));
 
-    // Copy data to device
-    cudaMemcpy(d_data, h_data.data(), n * sizeof(int), cudaMemcpyHostToDevice);
+//     // Copy data to device
+//     cudaMemcpy(d_data, h_data.data(), n * sizeof(int), cudaMemcpyHostToDevice);
 
-    int numBlocks = (n + (BLOCK_THREADS * ITEMS_PER_THREAD) - 1) / (BLOCK_THREADS * ITEMS_PER_THREAD);
+//     int numBlocks = (n + (BLOCK_THREADS * ITEMS_PER_THREAD) - 1) / (BLOCK_THREADS * ITEMS_PER_THREAD);
 
-    // Launch kernel to sort blocks
-    BlockSortKernel<<<numBlocks, BLOCK_THREADS>>>(d_data, d_sorted_data, n);
+//     // Launch kernel to sort blocks
+//     BlockSortKernel<<<numBlocks, BLOCK_THREADS>>>(d_data, d_sorted_data, n);
 
-    // Copy sorted data back to host
-    cudaMemcpy(h_data.data(), d_sorted_data, n * sizeof(int), cudaMemcpyDeviceToHost);
+//     // Copy sorted data back to host
+//     cudaMemcpy(h_data.data(), d_sorted_data, n * sizeof(int), cudaMemcpyDeviceToHost);
 
-    // Print sorted blocks
-    for (int i = 0; i < h_data.size(); i++) {
-        std::cout << h_data[i] << " ";
-    }
-    std::cout << std::endl;
+//     // Print sorted blocks
+//     for (int i = 0; i < h_data.size(); i++) {
+//         std::cout << h_data[i] << " ";
+//     }
+//     std::cout << std::endl;
 
-    // Free device memory
-    cudaFree(d_data);
-    cudaFree(d_sorted_data);
+//     // Free device memory
+//     cudaFree(d_data);
+//     cudaFree(d_sorted_data);
 
-    return 0;
-}
+//     return 0;
+// }
 
