324ea4fab5d5590aaaf9f76945b7fb66067ab4bf
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-06-18 19:11:25 +0800

    c

--- PkPkJoin/PkPKJoin.cu
+++ PkPkJoin/PkPKJoin.cu
@@ -10,9 +10,9 @@
 
 
 int main() {
-    std::vector<int> h_data(100);
+    std::vector<int> h_data(256);
     for (int i=0;i<h_data.size();i++) {
-        h_data[i]= rand() % 37;
+        h_data[i]=rand()%123;
     }
     int n = h_data.size();
 
@@ -30,16 +30,44 @@ int main() {
     // Launch kernel to sort blocks
     BlockSortKernel<<<numBlocks, BLOCK_THREADS>>>(d_data, d_sorted_data, n);
 
-    printArray<<<1,1>>>(d_sorted_data,n);
-
     int p = numBlocks;
-    int sample_size = n/p ;
-    
+    int sample_size = n / p;
     int *d_samples, *d_splitters;
     cudaMalloc(&d_samples, sample_size * sizeof(int));
     cudaMalloc(&d_splitters, (p - 1) * sizeof(int));
 
-    FindSplit(d_sorted_data,d_samples, d_splitters, n, p, sample_size);
+    FindSplit(d_sorted_data,d_samples, d_splitters, n, numBlocks, sample_size);
+    
+    // std::vector<int> h_data(100);
+    // for (int i=0;i<h_data.size();i++) {
+    //     h_data[i]= rand() % 37;
+    // }
+    // int n = h_data.size();
+
+    // // Allocate device memory
+    // int* d_data;
+    // cudaMalloc(&d_data, n * sizeof(int));
+    // int* d_sorted_data;
+    // cudaMalloc(&d_sorted_data, n * sizeof(int));
+
+    // // Copy data to device
+    // cudaMemcpy(d_data, h_data.data(), n * sizeof(int), cudaMemcpyHostToDevice);
+
+    // int numBlocks = (n + (BLOCK_THREADS * ITEMS_PER_THREAD) - 1) / (BLOCK_THREADS * ITEMS_PER_THREAD);
+
+    // // Launch kernel to sort blocks
+    // BlockSortKernel<<<numBlocks, BLOCK_THREADS>>>(d_data, d_sorted_data, n);
+
+    // printArray<<<1,1>>>(d_sorted_data,n);
+
+    // int p = numBlocks;
+    // int sample_size = n/p ;
+    
+    // int *d_samples, *d_splitters;
+    // cudaMalloc(&d_samples, sample_size * sizeof(int));
+    // cudaMalloc(&d_splitters, (p - 1) * sizeof(int));
+
+    // FindSplit(d_sorted_data,d_samples, d_splitters, n, p, sample_size);
 
     printArray<<<1,1>>>(d_samples,p-1);
 
