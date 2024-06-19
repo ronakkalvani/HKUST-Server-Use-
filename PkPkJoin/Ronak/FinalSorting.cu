86052b20acef2538df0f8f93e584b9838d32da4e
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-06-19 15:16:15 +0800

    c

--- PkPkJoin/Ronak/FinalSorting.cu
+++ PkPkJoin/Ronak/FinalSorting.cu
@@ -8,7 +8,7 @@
 #define ITEMS_PER_THREAD 1
 
 // Block-sorting CUDA kernel
-__global__ void BlockSortKernel(int *d_in, int *d_out, int *block_indices, int num_blocks, int num_elements)
+__global__ void BlockSortKernel2(int *d_in, int *d_out, int *block_indices, int num_blocks, int num_elements)
 {
     // Specialize BlockLoad, BlockStore, and BlockRadixSort collective types
     typedef cub::BlockLoad<int, BLOCK_THREADS, ITEMS_PER_THREAD, cub::BLOCK_LOAD_TRANSPOSE> BlockLoadT;
@@ -94,7 +94,7 @@ int main() {
     int numBlocks = h_block_indices.size();
 
     // Launch kernel to sort blocks
-    BlockSortKernel<<<numBlocks, BLOCK_THREADS>>>(d_data, d_sorted_data, d_block_indices, numBlocks, n);
+    BlockSortKernel2<<<numBlocks, BLOCK_THREADS>>>(d_data, d_sorted_data, d_block_indices, numBlocks, n);
 
     // Copy sorted data back to host
     cudaMemcpy(h_data.data(), d_sorted_data, n * sizeof(int), cudaMemcpyDeviceToHost);
