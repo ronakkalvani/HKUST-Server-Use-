28ae15b11d0ee6db2076e1627e74bbc94c858a16
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-06-19 13:55:41 +0800

    c

--- PkPkJoin/Ronak/FinalSorting.cu
+++ PkPkJoin/Ronak/FinalSorting.cu
@@ -47,40 +47,6 @@ __global__ void BlockSortKernel2(int *d_in, int *d_out, int *d_block_starts, int
 }
 
 
-// __global__ void BlockSortKernel2(int *d_in, int *d_out, int *d_block_starts, int num_blocks, int num_elements)
-// {
-//     // Specialize BlockLoad, BlockStore, and BlockRadixSort collective types
-//     typedef cub::BlockLoad<int, BLOCK_THREADS, ITEMS_PER_THREAD, cub::BLOCK_LOAD_VECTORIZE> BlockLoadT;
-//     typedef cub::BlockStore<int, BLOCK_THREADS, ITEMS_PER_THREAD, cub::BLOCK_STORE_VECTORIZE> BlockStoreT;
-//     typedef cub::BlockRadixSort<int, BLOCK_THREADS, ITEMS_PER_THREAD> BlockRadixSortT;
-
-//     // Allocate type-safe, repurposable shared memory for collectives
-//     __shared__ union {
-//         typename BlockLoadT::TempStorage load;
-//         typename BlockStoreT::TempStorage store;
-//         typename BlockRadixSortT::TempStorage sort;
-//     } temp_storage;
-
-//     // Determine the range of elements this block should sort
-//     int block_start = d_block_starts[blockIdx.x];
-//     int block_end = (blockIdx.x == num_blocks - 1) ? num_elements : d_block_starts[blockIdx.x + 1];
-//     int num_items = block_end - block_start;
-
-//     // Load data
-//     int thread_keys[ITEMS_PER_THREAD];
-//     BlockLoadT(temp_storage.load).Load(d_in + block_start, thread_keys, num_items);
-
-//     __syncthreads(); // Barrier for smem reuse
-
-//     // Collectively sort the keys
-//     BlockRadixSortT(temp_storage.sort).Sort(thread_keys);
-
-//     __syncthreads(); // Barrier for smem reuse
-
-//     // Store the sorted segment
-//     BlockStoreT(temp_storage.store).Store(d_out + block_start, thread_keys, num_items);
-// }
-
 // int main() {
 //     // Initialize host data
 //     std::vector<int> h_data(1024);
