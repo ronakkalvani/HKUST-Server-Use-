ddb5ac03cba887c1d2863835dcf7df87ce2f6b84
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-06-19 19:53:54 +0800

    c

--- PkPkJoin/Ronak/JoinAfterSort.cu
+++ PkPkJoin/Ronak/JoinAfterSort.cu
@@ -16,7 +16,7 @@ __global__ void JoinKernel(int * d_final_array, int* results, int numElements, i
         // Check if current element is equal to the next element
         if (d_final_array[tid] == d_final_array[tid + 1])
         {
-            int k=keys[tid];
+            int k=d_final_array[tid];
             results[3*tid] = k;
             results[3*tid+1] = hmap1[k];
             results[3*tid+2] = hmap2[k];
