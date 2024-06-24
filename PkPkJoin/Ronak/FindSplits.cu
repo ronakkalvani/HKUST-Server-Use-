d392ab663384e6770f95f5d722a73b4f2a10ce69
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-06-24 11:42:54 +0800

    c

--- PkPkJoin/Ronak/FindSplits.cu
+++ PkPkJoin/Ronak/FindSplits.cu
@@ -60,7 +60,7 @@ void FindSplit(int* d_sorted_data, int* d_samples, int* d_splitters, int n, int 
 int main() {
     const int n = 1e6;
     int p = 512;
-    int sample_size = p*log2(p);
+    int sample_size = p*int(log2(p));
 
     int h_sorted_subarrays[n];
     for (int i = 0; i < n; i++) {
