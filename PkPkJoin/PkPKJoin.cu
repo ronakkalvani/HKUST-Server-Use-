776f8faba91ecd9d5ca6739043e5424215abca38
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-07-02 18:57:37 +0800

    c

--- PkPkJoin/PkPKJoin.cu
+++ PkPkJoin/PkPKJoin.cu
@@ -26,7 +26,7 @@
 
 
 int main() {
-    int n1 = 1e2;
+    int n1 = 1e6;
     int n2 = 1e2;
 
     std::vector<int> keys1(n1);
@@ -39,7 +39,7 @@ int main() {
         keys2[i] = 3 * (i+1);
     }
 
-    int mx = 1e5;
+    int mx = 1e7;
     std::vector<int> hmap1(mx, 0);
     std::vector<int> hmap2(mx, 0);
 
