59806fa2c2e10d039c281bd48515abc8605f3ea5
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-07-03 16:33:01 +0800

    c

--- PkPkJoin/PkPKJoin.cu
+++ PkPkJoin/PkPKJoin.cu
@@ -34,7 +34,7 @@ int main() {
     for (int i = 1; i < n1; i++) {
         keys1[i] = (53*keys1[i-1]-5)%(n1+10);
     }
-    for (int i = 0; i < n2; i++) {
+    for (int i = 1; i < n2; i++) {
         keys2[i] = (71*keys1[i-1]-3)%(n2+10);
     }
 
