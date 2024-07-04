ac4c2fd4df343293efab196a00bb08f6c9cf916f
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-07-04 19:51:15 +0800

    c

--- PkPkJoin/CountSplitValues.cu
+++ PkPkJoin/CountSplitValues.cu
@@ -28,7 +28,7 @@ __global__ void countSplits(int* split_indices, int* split_counts, int num_eleme
 }
 
 int main() {
-    const int num_elements = 1e5;
+    const int num_elements = 1e6;
     const int block_size = 256;
     const int num_splits = num_elements/block_size;
     const int num_blocks = (num_elements + block_size - 1) / block_size;
