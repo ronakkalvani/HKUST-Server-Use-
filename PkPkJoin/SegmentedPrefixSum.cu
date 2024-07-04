a1d54b65c5c9bbedef4b7c2c0d7296ba905a132b
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-07-04 14:10:40 +0800

    c

--- PkPkJoin/SegmentedPrefixSum.cu
+++ PkPkJoin/SegmentedPrefixSum.cu
@@ -36,7 +36,7 @@ __global__ void segmentedPrefixSum(int *input, int *output, int n, int blockSize
 
 int main() {
     int blockSize = 512;
-    int n = 1e6;  // Large dataset size
+    int n = 1e3;  // Large dataset size
     std::vector<int> h_input(n);
 
     for (int i = 0; i < n; ++i) {
