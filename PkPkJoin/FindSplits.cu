bdcc3d3603c46f6c6050f43063bcc6a28de4931e
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-07-10 23:16:47 +0800

    c

--- PkPkJoin/FindSplits.cu
+++ PkPkJoin/FindSplits.cu
@@ -23,11 +23,6 @@ __global__ void Splitterss(int* d_splitters,int* d_samples,int sample_size,int p
     if (tid < p-1) {
         d_splitters[tid] = d_samples[(tid + 1) * sample_size / p];
     }
-    // if (tid==0) {
-    //     for(int i=0;i<p-1;i++) {
-    //         d_splitters[i] = d_samples[(i + 1) * sample_size / p];
-    //     }
-    // }
 }
 
 // Kernel to initialize the CURAND states
