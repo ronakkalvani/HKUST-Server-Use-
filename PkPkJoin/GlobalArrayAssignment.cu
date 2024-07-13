36869a9c21dfe76ba24414a085a322ed7949ee71
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-07-13 18:26:07 +0800

    c

--- PkPkJoin/GlobalArrayAssignment.cu
+++ PkPkJoin/GlobalArrayAssignment.cu
@@ -8,9 +8,6 @@ __global__ void Assign(int * NewBlockId,int* segment_sum,int* d_split_counts_pre
     if (tid < size) {
         int ind_in_cnt = blockIdx.x  + p * NewBlockId[tid];
         int final_ind = segment_sum[tid]+d_split_counts_prefixsum[ind_in_cnt];
-        if (d_dst[final_ind]!=0) {
-            printf("%d final index, %d segment, %d prefix, %d block, %d tid\n", final_ind,segment_sum[tid],d_split_counts_prefixsum[ind_in_cnt],NewBlockId[tid],tid);
-        }
         d_dst[final_ind] = d_src[tid];
     }
 
